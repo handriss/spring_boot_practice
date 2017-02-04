cd9a839b1b53d63ccc33b7aa3769fc92262ec385
handriss <andrashinkel@gmail.com>
2017-02-05 00:52:08 +0100

    beannames listed out

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for(String name : beanNames){
+            System.out.println(name);
+        }
 
 	}
 }
